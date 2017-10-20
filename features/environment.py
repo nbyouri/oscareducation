ae01720b65d86462832e9a9fede83d7262928b6e
Samuel Monroe <spat.monroe@gmail.com>
2017-10-20 13:29:41 +0200

    Behave working without django-behave

--- features/environment.py
+++ features/environment.py
@@ -1,8 +1,19 @@
 from datetime import datetime
 from selenium import webdriver
-
+import django
+from django.test.runner import DiscoverRunner
+from django.test.testcases import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.core import management
+from django.shortcuts import resolve_url
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'oscar.settings'
 
 def before_all(context):
+    django.setup()
+    context.test_runner = DiscoverRunner()
+    context.test_runner.setup_test_environment()
+    context.old_db_config = context.test_runner.setup_databases()
     # PhantomJS is used there (headless browser - meaning we can execute tests in a command-line environment,
     # which is what we want for use with SemaphoreCI
     # For debugging purposes, you can use the Firefox driver instead.
@@ -14,10 +25,40 @@ def before_all(context):
     # Using a different port, not conflicting if server is booted up
     context.server_url = 'http://localhost:8080'
 
+class BehaviorDrivenTestCase(StaticLiveServerTestCase):
+    """
+    Test case attached to the context during behave execution
+
+    This test case prevents the regular tests from running.
+    """
+
+    def runTest(*args, **kwargs):
+        pass
+
+
+def before_scenario(context, _):
+    context.test = BehaviorDrivenTestCase()
+    context.test.setUpClass()
+    context.test() # this starts a transaction
+
+    context.base_url = context.test.live_server_url
+
+    def get_url(to=None, *args, **kwargs):
+        return context.base_url + (
+            resolve_url(to, *args, **kwargs) if to else '')
+    context.get_url = get_url
+
+
+def after_scenario(context, _):
+    context.test.tearDownClass()
+    del context.test
+
 
 def after_all(context):
     # Explicitly quits the browser, otherwise it won't once tests are done
     context.browser.quit()
+    context.test_runner.teardown_databases(context.old_db_config)
+    context.test_runner.teardown_test_environment()
 
 
 def before_feature(context, feature):
