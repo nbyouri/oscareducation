a7737f83fcc700934bc07d3a11c9119681e4ef23
Samuel Monroe <spat.monroe@gmail.com>
2017-10-20 09:07:37 +0200

    Added django_behave in installed apps

--- oscar/settings.py
+++ oscar/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'behave_django',
     'authentification',
     'bootstrap3',
     'django_extensions',
