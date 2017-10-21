f019a73a2420c249aea5f5686cc446fc132da612
Samuel Monroe <spat.monroe@gmail.com>
2017-10-21 14:42:58 +0200

    Complete scenarios, reuse of simpler steps

--- features/login.feature
+++ features/login.feature
@@ -1,24 +1,43 @@
 Feature: Login
+  As a user
+  I want to be able to login into the site
+  In order to use its functionality
 
   Scenario: Login as a professor
 
-    Given an existing non logged professor
-    When I submit a valid login page
+    Given I am an existing non logged professor
+    Given I am on the login page
+    Then I enter my username
+    When I submit my username
+    Then I am on the password page
+    Then I enter my password
+    When I submit my password
     Then I am redirected to the professor home page
 
   Scenario: Login as a student
 
-    Given an existing non logged student
-    When I submit a valid login page
+    Given I am an existing non logged student
+    Given I am on the login page
+    Then I enter my username
+    When I submit my username
+    Then I am on the password page
+    Then I enter my password
+    When I submit my password
     Then I am redirected to the student home page
 
   Scenario: Login with wrong password
-
-    Given an existing non logged professor
-    When I submit an invalid password but valid account
+    Given I am an existing non logged professor
+    Given I am on the login page
+    Then I enter my username
+    When I submit my username
+    Then I am on the password page
+    Then I enter an invalid password
+    When I submit my password
     Then I am redirected to the login fail page
 
   Scenario: Login with wrong username
-    Given an existing non logged professor
-    When I submit an invalid username
+    Given I am an existing non logged professor
+    Given I am on the login page
+    Then I enter an invalid username
+    When I submit my username
     Then I am redirected to the login fail page
--- features/steps/login.py
+++ features/steps/login.py
@@ -1,29 +1,55 @@
 from behave import given, when, then
 
 
-@given('an existing non logged professor')
+@given('I am an existing non logged professor')
 def step_impl(context):
     from test.factories.user import ProfessorFactory
     u = ProfessorFactory.create()
     u.save()
 
 
-@given('an existing non logged student')
+@given('I am on the login page')
+def step_impl(context):
+    br = context.browser
+    br.get(context.base_url + '/accounts/usernamelogin/')
+    assert br.current_url.endswith('/accounts/usernamelogin/')
+
+
+@then('I enter my username')
+def step_impl(context):
+    br = context.browser
+    br.find_element_by_id("id_username").send_keys('username')
+
+
+@when('I submit my username')
+def step_impl(context):
+    br = context.browser
+    br.find_element_by_xpath("//input[@type='submit']").click()
+
+
+@then('I am on the password page')
+def step_impl(context):
+    br = context.browser
+    assert br.current_url.endswith('/accounts/passwordlogin/')
+
+
+@then('I enter my password')
+def step_impl(context):
+    br = context.browser
+    br.find_element_by_id("id_password").send_keys('password')
+
+
+@when('I submit my password')
+def step_impl(context):
+    br = context.browser
+    br.find_element_by_xpath("//input[@type='submit']").click()
+
+
+@given('I am an existing non logged student')
 def step_impl(context):
     from test.factories.user import StudentFactory
     u = StudentFactory.create()
     u.save()
-
-
-@when('I submit a valid login page')
-def step_impl(context):
-    br = context.browser
-    br.get(context.base_url + '/accounts/usernamelogin/')
-    br.find_element_by_id("id_username").send_keys('username')
-    br.find_element_by_xpath("//input[@type='submit']").click()
-    assert br.current_url.endswith('/accounts/passwordlogin/')
-    br.find_element_by_id("id_password").send_keys('password')
-    br.find_element_by_xpath("//input[@type='submit']").click()
 
 
 @then('I am redirected to the professor home page')
@@ -38,25 +64,19 @@ def step_impl(context):
     assert br.current_url.endswith('/student/dashboard/')
 
 
-@when('I submit an invalid username')
+@then('I enter an invalid username')
 def step_impl(context):
     br = context.browser
-    br.get(context.base_url + '/accounts/usernamelogin/')
     br.find_element_by_id("id_username").send_keys('WRONGUSERNAME')
-    br.find_element_by_xpath("//input[@type='submit']").click()
 
 
-@when('I submit an invalid password but valid account')
+@then('I enter an invalid password')
 def step_impl(context):
     br = context.browser
-    br.get(context.base_url + '/accounts/usernamelogin/')
-    br.find_element_by_id("id_username").send_keys('username')
-    br.find_element_by_xpath("//input[@type='submit']").click()
-    assert br.current_url.endswith('/accounts/passwordlogin/')
-    br.find_element_by_id("id_password").send_keys('wrongpassword')
-    br.find_element_by_xpath("//input[@type='submit']").click()
+    br.find_element_by_id("id_password").send_keys('WRONGPASSWORD')
 
 
+# FIXME : This distinction should not even occur as it gives to much information about whether a username is valid
 @then('I am redirected to the login fail page')
 def step_impl(context):
     br = context.browser
