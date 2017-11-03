621aa0a75fb08dca2dc1aade49c41ad5d5cc259e
Samuel Monroe <spat.monroe@gmail.com>
2017-11-03 23:37:44 +0100

    Arithmetic generator is backed up with feature test

--- features/generator.feature
+++ features/generator.feature
@@ -3,6 +3,10 @@ Feature: Generator
   I want to be able to automatically creates questions
   In order to easily populates my assessments
 
-  Scenario: Creating an arithmetic problem
+  Scenario: Successfully creating an arithmetic problem
 
     Given I am on the generator page
+    Then I enter a lower range
+    Then I enter an upper range
+    When I click on the create button
+    Then I see a list of generated problems
--- features/steps/generator.py
+++ features/steps/generator.py
@@ -7,3 +7,27 @@ def step_impl(context):
     br = context.browser
     br.get(context.base_url + '/questions_factory/generator/')
     assert br.current_url.endswith('/questions_factory/generator/')
+
+
+@then('I enter a lower range')
+def step_impl(context):
+    br = context.browser
+    br.find_element_by_id("id_range_from").send_keys('0')
+
+
+@then('I enter an upper range')
+def step_impl(context):
+    br = context.browser
+    br.find_element_by_id("id_range_to").send_keys('1000')
+
+
+@when('I click on the create button')
+def step_impl(context):
+    br = context.browser
+    br.find_element_by_id("id_generate_button").click()
+
+
+@then('I see a list of generated problems')
+def step_impl(context):
+    br = context.browser
+    assert br.find_element_by_id("id_generated_questions_page")
