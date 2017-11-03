a212a46053c59227922afa178acde67aad4ce4c1
Samuel Monroe <spat.monroe@gmail.com>
2017-11-04 00:38:46 +0100

    Form is validated by backend and frontend, custom validation, fully working

--- questions_factory/models/__init__.py
+++ questions_factory/models/__init__.py
@@ -1,4 +1,4 @@
 from .arithmetic_problem import Arithmetic_polynomial_second_degree
 from .problem_generator import Problem_generator
 from .problem_model import Problem_model
-from .problem_form import ProblemForm
+from .problem_form import *
--- questions_factory/views.py
+++ questions_factory/views.py
@@ -5,21 +5,20 @@ import json
 from django.core.exceptions import PermissionDenied
 from django.shortcuts import render
 from models.problem_generator import Problem_generator
+from models import *
 from models.problem_form import ArithmeticForm
 
 
 def generator(request):
-    if request.method == "GET":
-        form = ArithmeticForm()
-        return render(request, "questions_factory/settings_problems.haml", {'form': form})
+    form = Arithmetic_polynomial_second_degree.make_form(request.POST or None)
     if request.method == "POST":
-        # TODO Verify data send in POST
-        dom = request.POST['domain']
-        image = request.POST['image']
-        range = [int(request.POST['range_from']), int(request.POST['range_to'])]
-        problem_type = "Arithmetic_Polynomial_Second_degree"
-        data = {'problem': problem_type, 'image': image, 'domain': dom,
-                'range': range}
-        problem = Problem_generator.factory(json.dumps(data))
-        return render(request, "questions_factory/questions_list.haml", {'questions': problem.gen_questions(5)})
-    raise PermissionDenied()
+        if form.is_valid():
+            dom = form.cleaned_data['domain']
+            image = form.cleaned_data['image']
+            range = [int(form.cleaned_data['range_from']), int(form.cleaned_data['range_to'])]
+            problem_type = "Arithmetic_Polynomial_Second_degree"
+            data = {'problem': problem_type, 'image': image, 'domain': dom,
+                    'range': range}
+            problem = Problem_generator.factory(json.dumps(data))
+            return render(request, "questions_factory/questions_list.haml", {'questions': problem.gen_questions(5)})
+    return render(request, "questions_factory/settings_problems.haml", {'form': form})
