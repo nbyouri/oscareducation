33c2d505532b1224a2df5cb401884ebb9539b2bc
Samuel Monroe <spat.monroe@gmail.com>
2017-11-03 09:29:57 +0100

    A pre-defined form is now used instead of template-coded one

--- questions_factory/models/problem_form.py
+++ questions_factory/models/problem_form.py
@@ -1,12 +1,14 @@
 from django import forms
 
+DOMAIN_CHOICES = (('Natural', 'Naturels'), ('Integer', 'Entiers'), ('Rational', 'Rationnels'))
+IMAGE_CHOICES = (('Rational', 'Rationnels'), ('Complex', 'Complexes'), ('Integer', 'Entiers'))
+
 
 class ProblemForm(forms.Form):
+    domain = forms.ChoiceField(widget=forms.Select, choices=DOMAIN_CHOICES)
+    image = forms.ChoiceField(widget=forms.Select, choices=IMAGE_CHOICES)
 
-    def __init__(self):
-        super(ProblemForm, self).__init__()
-        domain_choices = (('Natural', 'Naturels'), ('Integer', 'Entiers'), ('Rational', 'Rationnels'))
-        self.domain = forms.ChoiceField(widget=forms.Select, domain=domain_choices)
-        image_choices = (('Natural', 'Naturels'), ('Rational', 'Rationnels'))
-        self.image = forms.ChoiceField(widget=forms.Select, image=image_choices)
 
+class ArithmeticForm(ProblemForm):
+    range_from = forms.FloatField(widget=forms.TextInput, required=True)
+    range_to = forms.FloatField(widget=forms.TextInput, required=True)
