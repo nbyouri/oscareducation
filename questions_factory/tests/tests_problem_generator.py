8b89355d06bc129b9a6525dfdb79117061853545
Samuel Monroe <spat.monroe@gmail.com>
2017-11-08 13:41:00 +0100

    Removing useless methods + renaming

--- questions_factory/tests/tests_problem_generator.py
+++ questions_factory/tests/tests_problem_generator.py
@@ -18,13 +18,8 @@ class UnexpectedBehaviour(TestCase):
         self.assertTrue('Wrong problem type' in context.exception)
 
 
-def new_arithmetic_dict():
-    dict = {"problem": "Arithmetic_Polynomial_Second_degree", "desc": "blabla"}
-    return dict
-
-
 def problem_settings(problem, problem_description, domain, image, range, val):
-    dict = {
+    settings = {
         "problem": problem,
         "desc": problem_description,
         "domain": domain,
@@ -32,22 +27,5 @@ def problem_settings(problem, problem_description, domain, image, range, val):
         "range": range
     }
     if val:
-        dict["val"] = val
-    return dict
-
-def write_json_file(data, path):
-    with open(path, 'w') as outfile:
-        json.dump(data, outfile)
-
-
-def read_json_file():
-    with open('json_tmp.txt') as json_file:
-        return json_file
-
-
-def remove_json_file(file):
-    try:
-        os.remove(file)
-    except:
-        OSError("File doesn't exist")
-
+        settings["val"] = val
+    return settings
