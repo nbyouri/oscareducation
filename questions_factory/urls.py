e56f154ca99720ea6e9f1e3ab10e856e0bdb6b6a
Willfrit <remy-voet@outlook.fr>
2017-11-21 11:23:05 +0100

    Begin receive good form depending of problem.

--- questions_factory/urls.py
+++ questions_factory/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     url(r'^generator/(?P<lesson_id>\d+)/(?P<skill_id>\d+)/(?P<test_id>\d+)/$', views.generator, name='generator'),
+    url(r'^generator/(?P<lesson_id>\d+)/(?P<skill_id>\d+)/(?P<test_id>\d+)/(?P<generator_name>\w+)/$', views.generator_choice, name='generator_choice'),
     url(r'^generator/(?P<lesson_id>\d+)/(?P<skill_id>\d+)/(?P<test_id>\d+)/submit/$', views.generator_submit, name='generator_submit')
 ]
