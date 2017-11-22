12d45d5901d280fa61988c59e172971951bf329b
Tristan <tristan.moers@student.uclouvain.be>
2017-11-22 13:05:05 +0100

    add average, median, standard deviantion calculcus

--- questions_factory/models/problem_models/statistics_problem.py
+++ questions_factory/models/problem_models/statistics_problem.py
@@ -1,15 +1,62 @@
 # coding=utf-8
 import random
+import numpy
+import yaml
 
-from questions_factory.models import Problem
+from collections import OrderedDict
+from examinations.models import *
+from questions_factory.models.problem_form import *
+from questions_factory.models.problem_models.problem import *
 
 
 class StatisticsProblem(Problem) :
-    def __init__(self, range=(0, 20)):
+    def __init__(self, nb=10, range=(0, 20)):
         self.range = range
         self.values = None
-
+        self.nb = nb
+        self.gen_values(self)
 
     def gen_values(self):
-        self.values = random.sample(range, 10)
+        self.values = random.sample(self.range, self.nb)
+
+
+    def get_sol(self):
+        sol = list()
+        average = self.get_average(self)
+        sol.append(average)
+        median = self.get_median(self)
+        sol.append(median)
+        standard_deviation = self.get_standard_deviation(self)
+        sol.append(standard_deviation)
+
+
+    def gen_questions(self, number_of_questions):
+        questions = list()
+        for _ in range(number_of_questions):
+            questions.append((self.new_question(self.get_sol())))
+            self.gen_values()
+        return questions
+
+    def new_question(self, sol):
+        question_desc = "Voici une série de données recueillies pour chaque jour écoulée durant" + str(self.nb) + " jours : "
+        + str(self.values)
+        answers = yaml.dump(OrderedDict([("answers", [sol]), ("type", "text")]))
+        question = Question(description=question_desc, answer=answers, source="Génerée automatiquement")
+        return question
+
+    """def make_form(self):"""
+
+
+
+    def get_average(self):
+        m = numpy.mean(self.values)
+        return m
+
+    def get_median(self):
+        m = numpy.median(self.values)
+        return m
+
+    def get_standard_deviation(self):
+        sd = numpy.std(self.values)
+        return sd
 
