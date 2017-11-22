a212e1da8b8364ba39965138b729e24a76064ae6
Youri Mouton <youri@NetBSD.org>
2017-11-22 17:41:49 +0100

    Add tests for volume problem.

--- questions_factory/tests/__init__.py
+++ questions_factory/tests/__init__.py
@@ -2,3 +2,4 @@ from tests_arithmetic_problem import *
 from tests_problem_generator import *
 from tests_requests import *
 from tests_simple_interest_problem import *
+from tests_volume_problem import *
