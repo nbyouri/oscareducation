b2b56a887ffe721fc4ef688eeac2f2775bacb07d
Willfrit <remy-voet@outlook.fr>
2017-11-23 16:59:00 +0100

    Refactor template (with partial).

--- questions_factory/views.py
+++ questions_factory/views.py
@@ -53,10 +53,11 @@ def generator(request, lesson_id, skill_id, test_id):
 
 @user_is_professor
 def generator_choice(request, lesson_id, skill_id, test_id, generator_name):
+    """Return partial html content"""
     form = get_form(generator_name, request)
     if not form:
         return HttpResponseNotFound('<h1>Erreur 404 : cannot generate this name of problem</h1>')
-    t = loader.get_template("questions_factory/generator_form.haml")
+    t = loader.get_template("questions_factory/_generator_form.haml")
     c = {'generator_name': generator_name, 'form': form}
     return HttpResponse(t.render(c, request))
 
